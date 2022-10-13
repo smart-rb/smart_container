7077882c74fefea6c56a011261aea187825a540c
Rustam Ibragimov <iamdaiver@gmail.com>
2022-10-13 22:28:52 +0300

    [decrease-memory-allocation] reduced mutex usages (use SmartCore::Engine::ReadWriteLock instead of raw mutexes

--- lib/smart_core/container.rb
+++ lib/smart_core/container.rb
@@ -7,6 +7,7 @@ require 'smart_core'
 module SmartCore
   # @api public
   # @since 0.1.0
+  # @version 0.10.0
   class Container # rubocop:disable Metrics/ClassLength
     require_relative 'container/version'
     require_relative 'container/errors'
@@ -82,13 +83,13 @@ module SmartCore
     #
     # @api public
     # @since 0.1.0
-    # @version 0.8.1
+    # @version 0.10.0
     def initialize(host_container: NO_HOST_CONTAINER, host_path: NO_HOST_PATH)
       @host = SmartCore::Container::Host.build(host_container, host_path)
       build_registry!
       @watcher = SmartCore::Container::DependencyWatcher.new(self)
       @host_path = host_path
-      @access_lock = SmartCore::Engine::ReadWriteLock.new
+      @lock = SmartCore::Engine::ReadWriteLock.new
     end
 
     # @param dependency_name [String, Symbol]
@@ -97,13 +98,13 @@ module SmartCore
     #
     # @api public
     # @sicne 0.1.0
-    # @version 0.8.0
+    # @version 0.10.0
     def register(
       dependency_name,
       memoize: SmartCore::Container::Registry::DEFAULT_MEMOIZATION_BEHAVIOR,
       &dependency_definition
     )
-      thread_safe do
+      @lock.write_sync do
         registry.register_dependency(dependency_name, memoize: memoize, &dependency_definition)
         watcher.notify(dependency_name)
       end
@@ -117,7 +118,7 @@ module SmartCore
     # @since 0.1.0
     # @version 0.8.0
     def namespace(namespace_name, &dependencies_definition)
-      thread_safe do
+      @lock.write_sync do
         registry.register_namespace(namespace_name, self, &dependencies_definition)
         watcher.notify(namespace_name)
       end
@@ -128,9 +129,9 @@ module SmartCore
     #
     # @api public
     # @since 0.1.0
-    # @version 0.1.0
+    # @version 0.10.0
     def resolve(dependency_path)
-      thread_safe { DependencyResolver.resolve(self, dependency_path) }
+      @lock.read_sync { DependencyResolver.resolve(self, dependency_path) }
     end
     alias_method :[], :resolve
 
@@ -139,32 +140,36 @@ module SmartCore
     #
     # @api public
     # @since 0.1.0
+    # @version 0.10.0
     def fetch(dependency_path)
-      thread_safe { DependencyResolver.fetch(self, dependency_path) }
+      @lock.read_sync { DependencyResolver.fetch(self, dependency_path) }
     end
 
     # @return [void]
     #
     # @api public
     # @since 0.1.0
+    # @version 0.10.0
     def freeze!
-      thread_safe { registry.freeze! }
+      @lock.write_sync { registry.freeze! }
     end
 
     # @return [Boolean]
     #
     # @api public
     # @since 0.1.0
+    # @version 0.10.0
     def frozen?
-      thread_safe { registry.frozen? }
+      @lock.read_sync { registry.frozen? }
     end
 
     # @return [void]
     #
     # @api public
     # @since 0.1.0
+    # @version 0.10.0
     def reload!
-      thread_safe { build_registry! }
+      @lock.write_sync { build_registry! }
     end
 
     # @option all_variants [Boolean]
@@ -172,8 +177,9 @@ module SmartCore
     #
     # @api public
     # @since 0.4.0
+    # @version 0.10.0
     def keys(all_variants: SmartCore::Container::Registry::DEFAULT_KEY_EXTRACTION_BEHAVIOUR)
-      thread_safe { registry.keys(all_variants: all_variants) }
+      @lock.read_sync { registry.keys(all_variants: all_variants) }
     end
 
     # @param key [String, Symbol]
@@ -181,8 +187,9 @@ module SmartCore
     #
     # @api public
     # @since 0.5.0
+    # @version 0.10.0
     def key?(key)
-      thread_safe { DependencyResolver.key?(self, key) }
+      @lock.read_sync { DependencyResolver.key?(self, key) }
     end
 
     # @param namespace_path [String, Symbol]
@@ -190,8 +197,9 @@ module SmartCore
     #
     # @api public
     # @since 0.5.0
+    # @version 0.10.0
     def namespace?(namespace_path)
-      thread_safe { DependencyResolver.namespace?(self, namespace_path) }
+      @lock.read_sync { DependencyResolver.namespace?(self, namespace_path) }
     end
 
     # @param dependency_path [String, Symbol]
@@ -200,8 +208,9 @@ module SmartCore
     #
     # @api public
     # @since 0.5.0
+    # @version 0.10.0
     def dependency?(dependency_path, memoized: nil)
-      thread_safe { DependencyResolver.dependency?(self, dependency_path, memoized: memoized) }
+      @lock.read_sync { DependencyResolver.dependency?(self, dependency_path, memoized: memoized) }
     end
 
     # @option yield_all [Boolean]
@@ -213,11 +222,12 @@ module SmartCore
     #
     # @api public
     # @since 0.4.0
+    # @version 0.10.0
     def each_dependency(
       yield_all: SmartCore::Container::Registry::DEFAULT_ITERATION_YIELD_BEHAVIOUR,
       &block
     )
-      thread_safe { registry.each_dependency(yield_all: yield_all, &block) }
+      @lock.read_sync { registry.each_dependency(yield_all: yield_all, &block) }
     end
     alias_method :each, :each_dependency
     alias_method :each_pair, :each_dependency
@@ -227,8 +237,9 @@ module SmartCore
     #
     # @api public
     # @since 0.1.0
+    # @version 0.10.0
     def hash_tree(resolve_dependencies: false)
-      thread_safe { registry.hash_tree(resolve_dependencies: resolve_dependencies) }
+      @lock.read_sync { registry.hash_tree(resolve_dependencies: resolve_dependencies) }
     end
     alias_method :to_h, :hash_tree
     alias_method :to_hash, :hash_tree
@@ -242,8 +253,9 @@ module SmartCore
     #
     # @api public
     # @since 0.8.0
+    # @version 0.10.0
     def observe(entity_path, &observer) # TODO: support for pattern-based pathes
-      thread_safe { watcher.watch(entity_path, &observer) }
+      @lock.write_sync { watcher.watch(entity_path, &observer) }
     end
     alias_method :subscribe, :observe
 
@@ -252,8 +264,9 @@ module SmartCore
     #
     # @api public
     # @since 0.8.0
+    # @version 0.10.0
     def unobserve(observer)
-      thread_safe { watcher.unwatch(observer) }
+      @lock.write_sync { watcher.unwatch(observer) }
     end
     alias_method :unsubscribe, :unobserve
 
@@ -262,8 +275,9 @@ module SmartCore
     #
     # @api public
     # @since 0.8.0
+    # @version 0.10.0
     def clear_observers(entity_path = nil) # TODO: support for pattern-based pathes
-      thread_safe { watcher.clear_listeners(entity_path) }
+      @lock.write_sync { watcher.clear_listeners(entity_path) }
     end
     alias_method :clear_listeners, :clear_observers
 
@@ -276,15 +290,6 @@ module SmartCore
     # @version 0.8.1
     def build_registry!
       @registry = RegistryBuilder.build(self)
-    end
-
-    # @param block [Block]
-    # @return [Any]
-    #
-    # @api private
-    # @since 0.1.0
-    def thread_safe(&block)
-      @access_lock.thread_safe(&block)
     end
   end
 end
